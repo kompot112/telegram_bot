39de5d12816d91a963a499e9aee935184828e138
kompot112 <149232543+kompot112@users.noreply.github.com>
2025-01-17 21:33:14 +0300

    config.py

--- config.py
+++ config.py
@@ -1,12 +1,1 @@
-import os
-
-BOT_TOKEN = "8038458965:AAHNaSWx_rmHYTVMgMtdzuCpx48GG2-KEdQ"
-OPENAI_API_KEY = "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"
-
-DB_HOST = "45.133.246.206"
-DB_PORT = "3306"
-DB_USER = "remote_user"
-DB_PASSWORD = "Qwerty5469"
-DB_NAME = "telegram_bot_db"
-FLASK_SERVER_URL = "http://45.133.246.206:5000"
 
